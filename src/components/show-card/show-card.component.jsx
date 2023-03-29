74c832f86cc664430a281b8c7e13d7247ff2715e
Probir Sarkar <81101648+probir-sarkar@users.noreply.github.com>
2023-03-29 06:18:46 +0530

    Update show-card.component.jsx

--- src/components/show-card/show-card.component.jsx
+++ src/components/show-card/show-card.component.jsx
@@ -3,28 +3,7 @@ import "./show-card.styles.scss";
 import Rating from "../rating/rating.component";
 
 const ShowCard = ({ show, index }) => {
-  //   return (
-  //     <div className="col-md-3 mb-4" key={show.id}>
-  //       <div className="card show-card h-100 p-2">
-  //         <img
-  //           src={show.image.medium}
-  //           className="card-img-top img-fluid"
-  //           alt="..."
-  //         />
-  //         <div className="card-body">
-  //           <h5 className="card-title">{show.name}</h5>
-  //           <p className="card-text">
-  //             {show.summary.replace(/(<([^>]+)>)/gi, "")}
-  //           </p>
-
-  //           <Link to={`/shows/${show.id}`} className="btn btn-primary">
-  //             Go to show
-  //           </Link>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   );
-  console.log(index);
+ if (!show.image) return null;
 
   return (
     <div className="show-card">
