0ffb6a9e86bb81618e20dd7933d6c4c9df396726
Probir Sarkar <Probirsarkar855@gmail.com>
2024-03-17 16:59:36 +0530

    speed improve using swr

--- src/contexts/tv-maze-api.context.jsx
+++ src/contexts/tv-maze-api.context.jsx
@@ -1,32 +1,27 @@
 // import useStates, useEffects, createContext, and axios
 import { useLocalStorage } from "@uidotdev/usehooks";
+import useSWR from "swr";
 import React, { useState, useEffect, createContext } from "react";
-import axios from "axios";
-
+const fetcher = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return response.json();
+};
 // create and export the context
 export const TvMazeContext = createContext();
 
 // create and export the provider
 export const TvMazeProvider = ({ children }) => {
-  // create a state
-  const [shows, setShows] = useState([]);
   const [bookedTickets, setBookedTickets] = useLocalStorage("bookedTickets", []);
-
-  // create a function to fetch data
-  const getShows = async () => {
-    const { data } = await axios.get("https://api.tvmaze.com/shows");
-    setShows(data);
-  };
-
-  // call the function in useEffect
-  useEffect(() => {
-    getShows();
-  }, []);
+  // SWR configuration
+  const { data: shows, error } = useSWR("https://api.tvmaze.com/shows", fetcher, { fallbackData: [] });
   const isBooked = (showId) => {
     return bookedTickets.find((ticket) => ticket.id === showId);
   };
 
-  const value = { shows, setShows, bookedTickets, setBookedTickets, isBooked };
+  const value = { shows, bookedTickets, setBookedTickets, isBooked };
 
   // return the provider
   return <TvMazeContext.Provider value={value}>{children}</TvMazeContext.Provider>;
